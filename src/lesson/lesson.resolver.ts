30ce4d2eed1c0358b3112901ebb5c038f2d993d2
josephdempsey <joeadempsey@gmail.com>
2021-04-29 18:22:58 +0100

    feat(nestjs-graphql-mongo): setup graphql with mongo

---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,35 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { AssignStudentsToLessonInput } from './assign-students-to-lesson.input';
+import { CreateLessonInput } from './lesson.input';
+import { LessonService } from './lesson.service';
+import { LessonType } from './lesson.type';
+
+@Resolver((of) => LessonType)
+export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+  @Query((returns) => LessonType)
+  lesson(@Args('id') id: string) {
+    return this.lessonService.getLesson(id);
+  }
+
+  @Query((returns) => [LessonType])
+  lessons() {
+    return this.lessonService.getLessons();
+  }
+
+  @Mutation((returns) => LessonType)
+  createLesson(
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
+  ) {
+    return this.lessonService.createLesson(createLessonInput);
+  }
+
+  @Mutation((returns) => LessonType)
+  assignStudentsToLesson(
+    @Args('assignStudentToLessonInput')
+    assignStudentsToLessonInput: AssignStudentsToLessonInput,
+  ) {
+    const { lessonId, studentIds } = assignStudentsToLessonInput;
+    return this.lessonService.assignStudentsToLesson(lessonId, studentIds);
+  }
+}
---
+++ src/student/student.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateStudentInput } from './create-student.input';
+import { Student } from './student.entity';
+import { v4 as uuid } from 'uuid';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class StudentService {
+  constructor(
+    @InjectRepository(Student) private studentRepository: Repository<Student>,
+  ) {}
+
+  async getStudents() {
+    return this.studentRepository.find();
+  }
+
+  async getStudent(id: string) {
+    return this.studentRepository.findOne({ id });
+  }
+
+  async createStudent(createStudentInput: CreateStudentInput) {
+    const { firstName, lastName } = createStudentInput;
+
+    const student = this.studentRepository.create({
+      id: uuid(),
+      firstName,
+      lastName,
+    });
+
+    return this.studentRepository.save(student);
+  }
+}
